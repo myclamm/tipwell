0decf9b00b15bb3985714b833c2e5c2f468a25a2
Mike Lam <mikelam231@gmail.com>
2016-09-28 23:26:24 -0700

    tippy app plus extra features are working

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,141 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Mike Lam on 9/27/16.
+//  Copyright © 2016 CodePath Assignment #1. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    // Refers to the default tip input
+    @IBOutlet weak var defaultTipIndex: UISegmentedControl!
+    
+    @IBOutlet weak var lowDefault: UITextField!
+    @IBOutlet weak var mediumDefault: UITextField!
+    @IBOutlet weak var highDefault: UITextField!
+    
+    // Default tip percentages
+    var tipPercentages: [Double] = [0.18,0.2,0.25]
+    
+    // Before view loads
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set default tip to be whatever's stored in UserDefault
+        let defaults = UserDefaults.standard
+        defaultTipIndex.selectedSegmentIndex = defaults.integer(forKey:"defaultTipIndex")
+        
+        // If user changed default tips, update tipPercentages
+        tipPercentages = defaults.array(forKey:"tipDefault") as? [Double] ?? tipPercentages
+        
+        // Set text values for custom tip fields
+        lowDefault.text = String(Int(tipPercentages[0]*100))
+        mediumDefault.text = String(Int(tipPercentages[1]*100))
+        highDefault.text = String(Int(tipPercentages[2]*100))
+        
+        updateDefaultTipIndex()
+    }
+    
+    // After view loads
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Invoked whenever default tip is changed
+    @IBAction func changeDefaultTip(_ sender: AnyObject) {
+        
+        // Store the new value in UserDefaults
+        let defaults = UserDefaults.standard
+        defaults.set(defaultTipIndex.selectedSegmentIndex, forKey: "defaultTipIndex")
+    }
+    
+    
+    @IBAction func onChangeLowValue(_ sender: AnyObject) {
+
+        changeDefaultTipValue(0)
+    }
+    
+    @IBAction func onChangeMediumValue(_ sender: AnyObject) {
+
+        changeDefaultTipValue(1)
+    }
+    
+    @IBAction func onChangeHighValue(_ sender: AnyObject) {
+        
+        changeDefaultTipValue(2)
+    }
+    
+    @IBAction func resetDefaults(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        tipPercentages = [0.18,0.2,0.25]
+        defaults.set(tipPercentages,forKey:"tipDefault")
+        lowDefault.text = String(Int(tipPercentages[0]*100))
+        mediumDefault.text = String(Int(tipPercentages[1]*100))
+        highDefault.text = String(Int(tipPercentages[2]*100))
+        changeDefaultTipValue(0)
+        changeDefaultTipValue(1)
+        changeDefaultTipValue(2)
+        
+    }
+    // Close the keyboard when user taps on view
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func changeDefaultTipValue(_ index: Int) {
+        var newVal: Double? = nil
+        switch index {
+        case 0:
+            newVal = Double(lowDefault.text!) ?? 0
+        case 1:
+            newVal = Double(mediumDefault.text!) ?? 0
+        case 2:
+            newVal = Double(highDefault.text!) ?? 0
+        default:
+            newVal = Double(lowDefault.text!) ?? 0
+        }
+        
+        print(newVal)
+        // Change settings view
+        let percentage:String = "\(Int(newVal!))%"
+        defaultTipIndex.setTitle(percentage, forSegmentAt: Int(index))
+        
+        // Change default settings
+        let defaults = UserDefaults.standard
+        
+        
+        var temp = defaults.array(forKey:"tipDefault") as? [Double] ?? tipPercentages
+        temp[index] = newVal!/100
+        defaults.set(temp,forKey:"tipDefault")
+    }
+    
+    func updateDefaultTipIndex() {
+        // Update defaultTipIndex view
+        for (i,v) in tipPercentages.enumerated() {
+            
+            let percentage:String = "\(Int(v*100))%"
+            
+            defaultTipIndex.setTitle(percentage, forSegmentAt: Int(i))
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tippy/TipViewController.swift
@@ -0,0 +1,129 @@
+//
+//  TipViewController.swift
+//  tippy
+//
+//  Created by Mike Lam on 9/27/16.
+//  Copyright © 2016 CodePath Assignment #1. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+    
+    
+    @IBOutlet weak var bar: UIView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    // Default tip percentages
+    var tipPercentages: [Double] = [0.18,0.2,0.25]
+    let white = UIColor(red:0.97, green:1.00, blue:0.97, alpha:1.0)
+    
+
+    let green = UIColor(red:0.17, green:0.92, blue:0.64, alpha:1.0)
+
+    // Before view loads
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Add cursor to bill field
+        billField.becomeFirstResponder()
+        let defaults = UserDefaults.standard
+        
+        // If user changed default tips in settings, update tipPercentages
+        tipPercentages = defaults.array(forKey:"tipDefault") as? [Double] ?? tipPercentages
+        
+        // Update tipControl view
+        for (i,v) in tipPercentages.enumerated() {
+            
+            let percentage:String = "\(Int(v*100))%"
+            
+            tipControl.setTitle(percentage, forSegmentAt: Int(i))
+        }
+        // Set the tip control to be whatever's stored in UserDefaults
+        tipControl.selectedSegmentIndex = defaults.integer(forKey:"defaultTipIndex")
+        // Recalculate tip
+        calculateTip()
+        animateBackground()
+        
+    }
+    
+
+    // After view loads
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        billField.textColor = self.white
+        billField.font = UIFont(name: "chalkduster", size: 50)
+        
+        tipLabel.textColor = self.white
+        totalLabel.textColor = self.white
+        label1.textColor = self.green
+        label2.textColor = self.green
+        
+        tipControl.tintColor = self.green
+        bar.backgroundColor = self.green
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Close the keyboard when user taps on view
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    // Because we need to recalculate tip when tip percent changes
+    @IBAction func onTipChange(_ sender: AnyObject) {
+        calculateTip()
+        animateBackground()
+    }
+    
+    // Because we need to recalculate tip when bill changes
+    @IBAction func onBillChange(_ sender: AnyObject) {
+        calculateTip()
+        
+    }
+    
+    func animateBackground () {
+        let tipIndex = self.tipControl.selectedSegmentIndex
+        UIView.animate(withDuration:
+            1.0, animations: {
+                
+                switch tipIndex {
+                case 2:
+                    self.mainView.backgroundColor = UIColor(red:0.13, green:0.67, blue:0.63, alpha:1.0)
+                    
+                case 1:
+                    self.mainView.backgroundColor = UIColor(red:0.27, green:0.41, blue:0.56, alpha:1.0)
+                case 0:
+                    self.mainView.backgroundColor = UIColor(red:0.02, green:0.11, blue:0.08, alpha:1.0)
+
+                default:
+                    self.mainView.backgroundColor = UIColor(red:0.02, green:0.11, blue:0.08, alpha:1.0)
+                    
+                }
+                
+                
+        })
+    }
+    // Calculate tip and render view changes
+    func calculateTip () {
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f",tip)
+        totalLabel.text = String(format: "$%.2f",total)
+    }
+    
+    
+}
+
